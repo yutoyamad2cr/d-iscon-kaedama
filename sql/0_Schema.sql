3a878e394e836af12d5e519a8eb49660822235f7
riimori <ryo.iimori@d2c.co.jp>
2025-07-05 13:32:31 +0900

    update: sql/0_Sschema.sql add index

--- sql/0_Schema.sql
+++ sql/0_Schema.sql
@@ -26,6 +26,11 @@ CREATE TABLE `isu_condition` (
   PRIMARY KEY(`id`)
 ) ENGINE=InnoDB DEFAULT CHARACTER SET=utf8mb4;
 
+-- isu_conditionの検索・集計高速化
+CREATE INDEX idx_isu_condition_jia_isu_uuid ON isu_condition (jia_isu_uuid);
+CREATE INDEX idx_isu_condition_jia_isu_uuid_timestamp ON isu_condition (jia_isu_uuid, timestamp DESC);
+CREATE INDEX idx_isu_condition_timestamp ON isu_condition (timestamp DESC);
+
 CREATE TABLE `user` (
   `jia_user_id` VARCHAR(255) PRIMARY KEY,
   `created_at` DATETIME(6) DEFAULT CURRENT_TIMESTAMP(6)
